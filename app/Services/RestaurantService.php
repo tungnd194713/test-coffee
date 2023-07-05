299a4b77f7904160daa0290590d1152066a6e802
tobakonek1 <tobakonek1@gmail.com>
2023-07-05 20:44:59 +0700

    rm dd

--- app/Services/RestaurantService.php
+++ app/Services/RestaurantService.php
@@ -68,7 +68,6 @@ class RestaurantService implements RestaurantServiceInterface
 
         $query = Comment::where('restaurant_id', $restaurant->id);
         if ($request->has('star_rating')) {
-            dd($request->star_rating);
             $query->where('star_rating', $request->star_rating);
         }
 
